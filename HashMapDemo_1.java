608f55781c91f1bdcbbc2336537d48aa770a4f8b
ShubhamJjadhav <officiallshubham@gmail.com>
2022-02-09 13:20:06 +0530

    Desscription for code changes

---
+++ HashMapDemo_1.java
@@ -0,0 +1,26 @@
+package com.capgemini.sfdc.col;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class HashMapDemo_1 
+{
+
+	public static void main(String[] args)
+	{
+
+	Map<String, String> stateCodes = new HashMap<>();
+
+	stateCodes.put("MH", "Maharashtra");
+	stateCodes.put("TS", "Telangana");
+	stateCodes.put("KA", "Karnataka");
+	stateCodes.put("DL", "Delhi");
+	stateCodes.put(null, "Andhra Pradesh");
+
+	System.out.println(stateCodes.get("KA"));
+	System.out.println(stateCodes.get(null));
+
+	}
+}
+
---
+++ HashMap_Exerc_1.java
@@ -0,0 +1,23 @@
+package com.capgemini.sfdc.col;
+
+import java.util.HashMap;
+
+public class HashMap_Exerc_1 {
+	
+	public static void main(String[] args)
+	{
+	
+		HashMap<String, Integer > map = new HashMap<>();
+		
+		map.put("Tokyo", 10);
+		map.put("Berlin", 30);
+		map.put("Professor", 20);
+
+		
+		System.out.println("Size of map is:- "+ map.size());
+
+		System.out.println( map );
+
+	}
+
+}
